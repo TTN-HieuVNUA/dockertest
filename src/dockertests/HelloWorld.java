8f6fb97981e4c90b0e425740b2ade4138dd69678
Hieu Johnny <hieutran2102000@gmail.com>
2022-03-08 13:23:24 +0700

    third

--- src/dockertests/HelloWorld.java
+++ src/dockertests/HelloWorld.java
@@ -1,8 +1,45 @@
 package dockertests;
 
-public class HelloWorld {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class HelloWorld extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HelloWorld frame = new HelloWorld();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelloWorld() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblHello = new JLabel("Hello");
+		lblHello.setBounds(181, 117, 80, 22);
+		contentPane.add(lblHello);
 	}
 }
